af4d62e73b105b2c9e71154e4e15ced27ca91e2e
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-15 05:05:02 +0600

    message deleting and room participants displaying added

--- base/urls.py
+++ base/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('create-room/', createRoom, name='create-room'),
     path('update-room/<str:pk>/', updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', deleteRoom, name='delete-room'),
+    path('delete-message/<str:pk>/', deleteMessage, name='delete-message'),
 ]
