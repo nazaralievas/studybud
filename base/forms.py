f19ba29c08f561c5aa2b423d0ca4756cda8fb694
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-16 20:53:02 +0600

    user update view and page added

--- base/forms.py
+++ base/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 from .models import *
 
 
@@ -6,3 +7,9 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = ['topic', 'name', 'description']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
--- base/urls.py
+++ base/urls.py
@@ -9,9 +9,11 @@ urlpatterns = [
     path('', home, name='home'),
 
     path('room/<str:pk>/', room, name='room'),
-    path('profile/<str:pk>/', userProfile, name='user-profile'),
     path('create-room/', createRoom, name='create-room'),
     path('update-room/<str:pk>/', updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', deleteRoom, name='delete-room'),
     path('delete-message/<str:pk>/', deleteMessage, name='delete-message'),
+
+    path('profile/<str:pk>/', userProfile, name='user-profile'),
+    path('update-user/', updateUser, name='update-user'),
 ]
