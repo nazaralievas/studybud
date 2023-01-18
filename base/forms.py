5b5f218683b48cc98de7622598060ea04c70cbb0
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-18 07:34:02 +0600

    user creation form altered

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,12 @@
 from django.forms import ModelForm
-from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room, User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
 
 
 class RoomForm(ModelForm):
@@ -11,4 +18,4 @@ class RoomForm(ModelForm):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
