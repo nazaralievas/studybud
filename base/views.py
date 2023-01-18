5ffbc4f8f3be5e0dedbe5ddf881aff066779027c
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-01-18 06:54:05 +0600

    customized user model with profile picture added

--- base/views.py
+++ base/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 
